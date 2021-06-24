d51f7869b14dafc5b0e1580db017f0c603c0fa8e
Aime julien BAUDOT <abaudot@e3r8p6.42.fr>
2021-06-24 21:15:08 +0200

    tester

--- tester/tester.py
+++ tester/tester.py
@@ -6,7 +6,7 @@
 #    By: abaudot <abaudot@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/06/23 17:05:45 by abaudot           #+#    #+#              #
-#    Updated: 2021/06/24 19:22:10 by abaudot          ###   ########.fr        #
+#    Updated: 2021/06/24 21:14:41 by abaudot          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,8 +14,8 @@ import subprocess
 from random import shuffle
 from itertools import permutations
 import sys
-exec(open("config.py").read())     
 
+exec(open("config.py").read())     
 
 class bcolors:
     HEADER = '\033[95m'
@@ -36,7 +36,7 @@ class bcolors:
     ENDC = '\033[0m'
 
 def all_perm(size):
-    max = 0;
+    max = 1;
     perm = list(permutations(['%d' %i for i in range(1, size + 1)]))
     for p in perm:
         ps = subprocess.run([path + "./push_swap", *p], stdout=subprocess.PIPE)
@@ -51,7 +51,7 @@ def all_perm(size):
     return (max)
 
 def sample_shuffle(size, n):
-    max = 0;
+    max = 1;
     l = ["%d" %i for i in range(size)];
     for i in range(n):
         shuffle(l);
@@ -67,8 +67,6 @@ def sample_shuffle(size, n):
     return (max)
 
 
-
-
 subprocess.run(["clear"], shell=True)
 subprocess.run(["echo ''"], shell=True)
 subprocess.run(["echo ''"], shell=True)
@@ -80,10 +78,14 @@ subprocess.run(["echo ''"], shell=True)
 subprocess.run(["echo ''"], shell=True)
 subprocess.run(["echo ''"], shell=True)
 
+print(bcolors.OKBLUE + "Info: consult config.py for quick test personalisation" + bcolors.ENDC)
+subprocess.run(["echo ''"], shell=True)
+subprocess.run(["echo ''"], shell=True)
+
 print(bcolors.HEADER + "0)\tcompiling:" + bcolors.ENDC)
 subprocess.run(["make -C" + path], shell=True)
 
-print(bcolors.HEADER + "\n1)\ttesting all solutions relative to 3 elements:" + bcolors.ENDC)
+print(bcolors.HEADER + "\n1)\ttesting all solutions relative to 3 elements stack:" + bcolors.ENDC)
 tt = all_perm(3)
 if tt > 2 or tt == 0:
     print (bcolors.FAIL + "your mark: 0" + bcolors.ENDC)
@@ -91,7 +93,7 @@ if tt > 2 or tt == 0:
 else:
     print (bcolors.OKGREEN + "OK!\tmax: ", tt, bcolors.ENDC)
 
-print(bcolors.HEADER + "\n2)\ttesting all solutions relative to 5 elements:" + bcolors.ENDC)
+print(bcolors.HEADER + "\n2)\ttesting all solutions relative to 5 elements stack:" + bcolors.ENDC)
 tt = all_perm(5)
 if tt > 12 or tt == 0:
     print (bcolors.FAIL + "your mark: 0" + bcolors.ENDC)
@@ -99,10 +101,11 @@ if tt > 12 or tt == 0:
 else:
     print (bcolors.OKGREEN + "OK!\tmax: ", tt, bcolors.ENDC)
 
-print(bcolors.HEADER + "\n3)\ttesting a " + str(nb_of_test) + " random samble for 100 elements:" + bcolors.ENDC)
+print(bcolors.HEADER + "\n3)\ttesting a " + bcolors.OKBLUE +  str(nb_of_test) + bcolors.HEADER + " random samble for 100 elements stack:" + bcolors.ENDC)
 tt = sample_shuffle(100, nb_of_test)
 if (tt == 0 or tt >= 1500):
     print (bcolors.FAIL + "your mark: 0" + bcolors.ENDC)
+    exit()
 elif (tt < 700):
     print (bcolors.OKGREEN + "OK!\t max: ", tt, "\tscore: 5/5" + bcolors.ENDC)
 elif (tt < 900):
@@ -114,10 +117,11 @@ elif (tt < 1300):
 elif (tt < 1300):
     print (bcolors.WARNING + "OK!\t max: ", tt, "\tscore: 1/5" + bcolors.ENDC)
 
-print(bcolors.HEADER + "\n4)\ttesting a " + str(nb_of_test) + " random samble for 500 elements:" + bcolors.ENDC)
+print(bcolors.HEADER + "\n4)\ttesting a " + bcolors.OKBLUE + str(nb_of_test) + bcolors.HEADER + " random samble for 500 elements stack:" + bcolors.ENDC)
 tt = sample_shuffle(500, nb_of_test)
 if (tt == 0 or tt >= 11500):
     print (bcolors.FAIL + "your mark: 0" + bcolors.ENDC)
+    exit()
 elif (tt < 5500):
     print (bcolors.OKGREEN + "OK!\t max: ", tt, "\tscore: 5/5" + bcolors.ENDC)
 elif (tt < 7000):
@@ -129,24 +133,15 @@ elif (tt < 10000):
 elif (tt < 11500):
     print (bcolors.WARNING + "OK!\t max: ", tt, "\tscore: 1/5" + bcolors.ENDC)
 
-print(bcolors.HEADER + "\n4)\t Final testing 2 random sambles for [2 -> " + str(upper_limit) + "] elements:" + bcolors.ENDC)
-#load1 = upper_limit / 10
-#load2 = upper_limit / 3
-#load3 = upper_limit / 2
-#load4 = upper_limit - load2
-for i in range(2, 1501) :
+print(bcolors.HEADER + "\n4)\t Final testing 2 random sambles for [1 -> " + bcolors.OKBLUE + str(upper_limit) + bcolors.HEADER + "] elements stack:" + bcolors.ENDC)
+
+for i in range(1, upper_limit + 1) :
     tt = sample_shuffle(i, 2)
     if tt == 0 :
-        print (bcolors.FAIL + "fail " + str(i) + "test:\tyour mark: 0" + bcolors.ENDC)
+        print (bcolors.FAIL + "fail the " + str(i) + " nd test:\tyour mark: 0" + bcolors.ENDC)
         quit()
- #   if i == load1:
- #       subprocess.run(["echo -ne"], input =  bcolors.PURPLE + "\t|###" + bcolors.YELOW + "...................." + bcolors.ENDC, shell=True)
- #   elif i == load2:
- #       subprocess.run(["echo", "-ne", bcolors.PURPLE + "\t|######" + bcolors.YELOW + "................." + bcolors.ENDC], shell=True)
- #   elif i == load3:
- #      subprocess.run(["echo", "-ne", bcolors.PURPLE + "\t|#########" + bcolors.YELOW + "..............." + bcolors.ENDC], shell=True)
- #   elif i == load4:
- #      subprocess.run(["echo", "-ne", bcolors.PURPLE + "\t|############" + bcolors.YELOW + "............" + bcolors.ENDC], shell=True)
 
-
-print (bcolors.OKGREEN + "OK!\t all tests passed !" + bcolors.ENDC)
+print (bcolors.OKGREEN + "OK!\t all tests passed !")
+subprocess.run(["echo ''"], shell=True)
+subprocess.run(["echo ''"], shell=True)
+subprocess.run(["cat cat"], shell=True)
